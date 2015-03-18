4a9b458830a0b4b9ce805fb11e9c0f8e8c804073
Sam Bell <sam@idealwebsites.co.uk>
2015-03-18 20:12:01 +0000

    Responsive Gohper

--- js/contact.js
+++ js/contact.js
@@ -1,97 +1,5 @@
 $(function() {
 	'use strict';
-	// Main contact form
-    $('#contact').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-                required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            email: {
-                required: "Please enter your email address"
-            },
-            message: {
-                required: "Please enter your message",
-                minlength: "Your message must consist of at least 2 characters"
-            }
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"inc/contact.php",
-                success: function() {
-                    $('#contact :input').attr('disabled', 'disabled');
-                    $('#contact').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#contact').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
-    });
-    
-    // Signup form
-    $('#signup').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            email: {
-                required: "Please enter your email address"
-            }
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"inc/signup.php",
-                success: function() {
-                    $('#signup :input').attr('disabled', 'disabled');
-                    $('#signup').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#signup').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
-    });
 	
 	// Subscription form
    	$('#subscribe').validate({
@@ -110,7 +18,7 @@ $(function() {
             $(form).ajaxSubmit({
                 type:"POST",
                 data: $(form).serialize(),
-                url:"inc/subscribe.php",
+                url:"http://www.sam-bell.com/golanguk/inc/subscribe.php",
                 success: function() {
                     $('#subscribe :input').attr('disabled', 'disabled');
                     $('#subscribe').fadeTo( "slow", 0.15, function() {
